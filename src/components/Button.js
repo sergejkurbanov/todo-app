0b390c4ce67ec57243737785ba211f920fe01811
Sergej Kurbanov <sergejkurbanov@gmail.com>
2019-03-06 23:59:35 +0100

    eslint replaced by prettier

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,13 +8,15 @@ const Button = styled.button`
   margin: 0.5em 1em;
   padding: 0.25em 1em;
   &:hover {
-    color: 
+    color: ;
   }
 
-  ${props => props.primary && css`
-    background: palevioletred;
-    color: white;
-  `}
+  ${props =>
+    props.primary &&
+    css`
+      background: palevioletred;
+      color: white;
+    `}
 `
 
 export default Button
