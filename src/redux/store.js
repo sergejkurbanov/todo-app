f77a53d005c18c6ed1f4e4e1f2fbfdf04569afe3
Sergej Kurbanov <sergejkurbanov@gmail.com>
2019-04-19 18:47:22 +0200

    redux dev tools added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import logger from 'redux-logger'
 import rootReducer from './modules/rootReducer'
 import rootSaga from './modules/rootSaga'
 
 const sagaMiddleware = createSagaMiddleware()
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware, logger))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware, logger)),
+)
 
 sagaMiddleware.run(rootSaga)
 
